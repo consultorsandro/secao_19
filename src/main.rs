3d9b42d6842ee30794475cb0d7a5cf173e5be9df
Sandro Reis <consultorsandro@hotmail.com>
2025-07-31 09:08:06 -0300

    Class 335 e 336 Adiciona função para selecionar os dois primeiros elementos de um vetor de Strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn select_first_two_elements<'a>(items: &'a [String]) -> &'a [String] {
+    &items[0..2]
+}
+fn main() { // Class 335 and 336 lifetimes
+    let cities = vec![
+        String::from("New York"),
+        String::from("Los Angeles"),
+        String::from("Chicago"),
+    ];
+    let two_cities = {
+        let cities_reference = &cities;
+        select_first_two_elements(cities_reference)
+    };
+    println!("{:?}", two_cities);
+
+    {
+        let coffees = [String::from("Latte"), String::from("mocha")];
+        let two_coffees = select_first_two_elements(&coffes);
+        println!("{:?}", two_coffees);
+
+
+    }
+
+}
+/*
 fn main() { // Class 329 lifetimes
     let dog = String::from("Watson");
 
@@ -13,3 +38,4 @@ println!("{}", dog);
 }
 
 }
+*/
